4b06168e71f5b316e714083419167ea60263960d
matteo-brandolino <matteo.brandolino@gmail.com>
2023-12-26 20:31:25 +0100

    update readme and fix middleware

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,16 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "event/:id",
-    "api/webhook/clerk",
-    "api/webhook/stripe",
-    "api/uploadthing",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
   ],
-  ignoredRoutes: ["api/webhook/clerk", "api/webhook/stripe", "api/uploadthing"],
+  ignoredRoutes: [
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
 });
 
 export const config = {
