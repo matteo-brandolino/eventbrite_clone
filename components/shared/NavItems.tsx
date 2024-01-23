bdb5316da951397f126af2b983fca2a4ebac0bde
matteo-brandolino <matteo.brandolino@gmail.com>
2024-01-23 21:36:19 +0100

    add events details and stripe

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -11,7 +11,6 @@ const NavItems = () => {
     <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
       {headerLinks.map((link) => {
         const isActive = pathname === link.route;
-        console.log(isActive);
 
         return (
           <li
